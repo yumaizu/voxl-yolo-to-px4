f6352dfb944fb1aaab157d59e7c9640dbdea5cef
Yuma Tezuka <s1302010@u-aizu.ac.jp>
2026-07-21 14:31:02 +0900

    Fixed incorrect return code

--- YOLOReceiver.py
+++ YOLOReceiver.py
@@ -51,7 +51,7 @@ class YOLOReceiver(Node):
 
         # Wait for voxl-tflite-server to start before starting voxl_mpa_to_ros2
         stat =subprocess.run(['systemctl', 'is-active', '--quiet', 'voxl-tflite-server']).returncode
-        while stat != 0:
+        while stat != 1:
             self.get_logger().info(
                 'Waiting for voxl-tflite-server to start...'
             )
@@ -172,7 +172,7 @@ class YOLOReceiver(Node):
         )
         # Wait for voxl-tflite-server to stop before destroying the node
         stat =subprocess.run(['systemctl', 'is-active', '--quiet', 'voxl-tflite-server']).returncode
-        while stat != 1:
+        while stat != 0:
             self.get_logger().info(
                 'Waiting for voxl-tflite-server to stop...'
             )
